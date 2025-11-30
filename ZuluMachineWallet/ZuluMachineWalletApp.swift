38e6449e720416198b64d62f490e2c7148507eb2
Sipho Motha <sipho.motha@standardbank.co.za>
2025-11-30 12:29:53 +0200

    Initial Commit

---
+++ ZuluMachineWallet/ZuluMachineWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZuluMachineWalletApp.swift
+//  ZuluMachineWallet
+//
+//  Created by Sipho Motha on 2025/11/30.
+//
+
+import SwiftUI
+
+@main
+struct ZuluMachineWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
